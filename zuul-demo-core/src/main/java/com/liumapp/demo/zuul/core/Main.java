42c890a586d656487242aec90f2bed7a1362067f
liumapp <liumapp.com@gmail.com>
2018-02-06 10:43:23 +0800

    enabel zuul proxy

--- zuul-demo-core/src/main/java/com/liumapp/demo/zuul/core/Main.java
+++ zuul-demo-core/src/main/java/com/liumapp/demo/zuul/core/Main.java
@@ -2,13 +2,15 @@ package com.liumapp.demo.zuul.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 2/6/18 10:43 AM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
+@EnableZuulProxy
+@SpringBootApplication(scanBasePackages = {"com.liumapp.demo.zuul.core"})
 public class Main {
 
     public static void main(String[] args) {
