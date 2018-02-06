b74578064d55ffbbadf8b369f166f18925346c16
liumapp <liumapp.com@gmail.com>
2018-02-06 15:29:08 +0800

    filter fix

--- zuul-demo-gateway/src/main/java/com/liumapp/demo/zuul/gateway/filter/AccessFilter.java
+++ zuul-demo-gateway/src/main/java/com/liumapp/demo/zuul/gateway/filter/AccessFilter.java
@@ -2,6 +2,12 @@ package com.liumapp.demo.zuul.gateway.filter;
 
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 /**
  * Created by liumapp on 2/6/18.
@@ -10,9 +16,18 @@ import com.netflix.zuul.ZuulFilter;
  */
 public class AccessFilter extends ZuulFilter {
 
+    private static Logger log = LoggerFactory.getLogger(AccessFilter.class);
+
+    /**
+     *  pre : before
+     *  routing : while routing
+     *  post : after routing and error
+     *  error : catch exception
+     * @return String
+     */
     @Override
     public String filterType() {
-        return null;
+        return "pre";
     }
 
     @Override
@@ -22,11 +37,25 @@ public class AccessFilter extends ZuulFilter {
 
     @Override
     public boolean shouldFilter() {
-        return false;
+        return true;
     }
 
     @Override
     public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        Object accessToken = request.getParameter("accessToken");
+        if(accessToken == null) {
+            log.warn("access token is empty");
+            // or use ctx.setResponseBody(body) , that dependence on you
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            return null;
+        }
+        log.info("access token ok");
         return null;
     }
 
